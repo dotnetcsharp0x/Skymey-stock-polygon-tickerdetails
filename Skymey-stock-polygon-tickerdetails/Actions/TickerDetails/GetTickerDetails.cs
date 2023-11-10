d58ad825692a32b97b0f516318ed020e04214588
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-10 10:52:38 +0300

    Добавьте файлы проекта.

---
+++ Skymey-stock-polygon-tickerdetails/Actions/TickerDetails/GetTickerDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skymey_stock_polygon_tickerdetails.Actions.TickerDetails
+{
+    public class GetTickerDetails
+    {
+
+    }
+}
---
+++ Skymey-stock-polygon-tickerdetails/Data/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using MongoDB.EntityFrameworkCore.Extensions;
+using Skymey_main_lib.TickerDetails.Polygon;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Skymey_stock_polygon_tickerdetails.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<TickerDetails> TickerDetails { get; init; }
+        public static ApplicationContext Create(IMongoDatabase database) =>
+            new(new DbContextOptionsBuilder<ApplicationContext>()
+                .UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName)
+                .Options);
+        public ApplicationContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TickerDetails>().ToCollection("stock_tickerdetails");
+        }
+    }
+}
